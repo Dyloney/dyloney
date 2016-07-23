389bd08d3390d2ca7995520fd41a0baa8adc5ba0
aransfor <ransfordanthony@gmail.com>
2016-07-23 11:14:31 -0700

    changed indenting

--- main.py
+++ main.py
@@ -46,13 +46,13 @@ usedcat = []
 def populate_tree_view(tv):
 
     for i, item in enumerate(foodlist):
-        g = tv.add_node(TreeViewLabel(text='%s' % item.categ, font_size = '50sp'))
+        g = tv.add_node(TreeViewButton(text='%s' % item.categ, font_size = '50sp'))
         tv.add_node(TreeViewButton(text='%s' % item.name, font_size = '40sp'), g)
 
 class POSFMApp(App):
 
     def build(self):
-        tv = TreeView(root_options=dict(text='Tree One'), hide_root=True, indent_level=4)
+        tv = TreeView(root_options=dict(text='Tree One'), hide_root=True, indent_level=100, indent_start=200)
         tv.size_hint = 1, None
         tv.bind(minimum_height = tv.setter('height'))
         populate_tree_view(tv)
