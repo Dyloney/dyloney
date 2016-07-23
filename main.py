beb73f7604a46ed7f9939eec53124a74d8d9f7f8
aransfor <ransfordanthony@gmail.com>
2016-07-23 13:14:25 -0700

    node buttons clickable

--- main.py
+++ main.py
@@ -43,22 +43,28 @@ foodlist.append(fooditem('Meat', 'Pork Chops'))
 foodlist.append(fooditem('Bread', 'rolls'))
 usedcat = []
 
-def populate_tree_view(tv):
-
-    for i, item in enumerate(foodlist):
-        g = tv.add_node(TreeViewButton(text='%s' % item.categ, font_size = '50sp'))
-        tv.add_node(TreeViewButton(text='%s' % item.name, font_size = '40sp'), g)
 
 class POSFMApp(App):
 
     def build(self):
-        tv = TreeView(root_options=dict(text='Tree One'), hide_root=True, indent_level=100, indent_start=200)
-        tv.size_hint = 1, None
-        tv.bind(minimum_height = tv.setter('height'))
-        populate_tree_view(tv)
+        self.tv = TreeView(root_options=dict(text='Tree One'), hide_root=True, indent_level=0, indent_start=0)
+        self.tv.size_hint = 1, None
+        self.tv.bind(minimum_height = self.tv.setter('height'))
+        self.populate_tree_view(self.tv)
         root = ScrollView(pos = (0, 0))
-        root.add_widget(tv)
+        root.add_widget(self.tv)
         return root
+
+    def populate_tree_view(self, tv):
+
+        for i, item in enumerate(foodlist):
+	    catbutton = TreeViewButton(text='%s' % item.categ, font_size = '50sp', background_color=[1,1,0,1])
+	    catbutton.bind(on_press=self.cat_clicked)
+            g = self.tv.add_node(catbutton)
+            self.tv.add_node(TreeViewButton(text='%s' % item.name, font_size = '40sp'), g)
+
+    def cat_clicked(self, button):
+        self.tv.toggle_node(button)
 
 if __name__ == '__main__':
     POSFMApp().run()
