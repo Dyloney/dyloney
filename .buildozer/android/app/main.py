4064c284425cc3a8028c8ad37ce8871691b96053
aransfor <ransfordanthony@gmail.com>
2016-07-24 12:14:00 -0700

    menu working

--- .buildozer/android/app/main.py
+++ .buildozer/android/app/main.py
@@ -1,5 +1,5 @@
 from kivy.uix.scrollview import ScrollView
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.treeview import TreeView, TreeViewNode
@@ -19,49 +19,76 @@ class fooditem():
 foodlist = []
 foodlist.append(fooditem('Fruit', 'apple'))
 foodlist.append(fooditem('Vegetable', 'carrots'))
-foodlist.append(fooditem('Meat', 'Pork Chops'))
-foodlist.append(fooditem('Bread', 'rolls'))
-foodlist.append(fooditem('Fruit', 'apple'))
-foodlist.append(fooditem('Vegetable', 'carrots'))
-foodlist.append(fooditem('Meat', 'Pork Chops'))
-foodlist.append(fooditem('Bread', 'rolls'))
-foodlist.append(fooditem('Fruit', 'apple'))
-foodlist.append(fooditem('Vegetable', 'carrots'))
-foodlist.append(fooditem('Meat', 'Pork Chops'))
-foodlist.append(fooditem('Bread', 'rolls'))
-foodlist.append(fooditem('Fruit', 'apple'))
-foodlist.append(fooditem('Vegetable', 'carrots'))
-foodlist.append(fooditem('Meat', 'Pork Chops'))
-foodlist.append(fooditem('Bread', 'rolls'))
-foodlist.append(fooditem('Fruit', 'apple'))
-foodlist.append(fooditem('Vegetable', 'carrots'))
-foodlist.append(fooditem('Meat', 'Pork Chops'))
-foodlist.append(fooditem('Bread', 'rolls'))
-foodlist.append(fooditem('Fruit', 'apple'))
-foodlist.append(fooditem('Vegetable', 'carrots'))
-foodlist.append(fooditem('Meat', 'Pork Chops'))
-foodlist.append(fooditem('Bread', 'rolls'))
-usedcat = []
+foodlist.append(fooditem('Meat', 'Pork chops'))
+foodlist.append(fooditem('Wheat/Grains', 'rolls'))
+foodlist.append(fooditem('Fruit', 'pears'))
+foodlist.append(fooditem('Vegetable', 'cucumber'))
+foodlist.append(fooditem('Meat', 'bacon'))
+foodlist.append(fooditem('Wheat/Grains', 'wheat'))
+foodlist.append(fooditem('Fruit', 'orange'))
+foodlist.append(fooditem('Vegetable', 'celery'))
+foodlist.append(fooditem('Meat', 'beef'))
+foodlist.append(fooditem('Wheat/Grains', 'white'))
+foodlist.append(fooditem('Fruit', 'cantelope'))
+foodlist.append(fooditem('Vegetable', 'tomato'))
+foodlist.append(fooditem('Meat', 'hot dogs'))
+foodlist.append(fooditem('Wheat/Grains', 'cake'))
+foodlist.append(fooditem('Fruit', 'banana'))
+foodlist.append(fooditem('Vegetable', 'onion'))
+foodlist.append(fooditem('Meat', 'hamburger'))
+foodlist.append(fooditem('Wheat/Grains', 'hotdog buns'))
+foodlist.append(fooditem('Fruit', 'lemon'))
+foodlist.append(fooditem('Vegetable', 'green beans'))
+foodlist.append(fooditem('Meat', 'ribs'))
+foodlist.append(fooditem('Wheat/Grains', 'french rolls'))
+foodlist.append(fooditem('Miscellaneous', 'batteries'))
+foodlist.append(fooditem('Dairy', 'milk'))
+foodlist.append(fooditem('Dairy', 'eggs'))
+foodlist.append(fooditem('Frozen Goods', 'tv dinners'))
+foodlist.append(fooditem('Frozen Goods', 'hot Pockets'))
+
+usedcat = {}
 
 
 class POSFMApp(App):
 
     def build(self):
+	layout = FloatLayout()
         self.tv = TreeView(root_options=dict(text='Tree One'), hide_root=True, indent_level=0, indent_start=0)
         self.tv.size_hint = 1, None
         self.tv.bind(minimum_height = self.tv.setter('height'))
         self.populate_tree_view(self.tv)
+
+	self.camera = Button(text = 'Camera', size_hint =(1/3.,.23),
+				background_color=[1,0,0,.6], pos_hint={'x':0,'y':0})
+	self.add = Button(text = 'Add', size_hint =(1/3.,.23),
+			background_color=[1,0,0,.6], pos_hint={'x':1/3.,'y':0})
+	self.sort = Button(text = 'Sort', size_hint =(1/3.,.23), 
+			   background_color=[1,0,0,.6], pos_hint={'x':2/3.,'y':0})
         root = ScrollView(pos = (0, 0))
         root.add_widget(self.tv)
-        return root
+	layout.add_widget(root)
+	layout.add_widget(self.camera)
+	layout.add_widget(self.add)
+	layout.add_widget(self.sort)
+	#root.add_widget(self.menu)
+        return layout
 
     def populate_tree_view(self, tv):
 
         for i, item in enumerate(foodlist):
-	    catbutton = TreeViewButton(text='%s' % item.categ, font_size = '50sp', background_color=[1,1,0,1])
-	    catbutton.bind(on_press=self.cat_clicked)
-            g = self.tv.add_node(catbutton)
-            self.tv.add_node(TreeViewButton(text='%s' % item.name, font_size = '40sp'), g)
+	    if item.categ not in usedcat.keys():
+	        catbutton = TreeViewButton(text='%s' % item.categ, font_size = '50sp',
+				size = (100, 450), background_color=[1,1,0,1])
+	        catbutton.bind(on_press=self.cat_clicked)
+                g = self.tv.add_node(catbutton)
+		usedcat[item.categ] = g
+	    else:
+		g = usedcat[item.categ]
+	    itembutton = TreeViewButton(text='%s' % item.name, font_size = '30sp',
+				size = (100,150), background_color=[0,1,1,1])
+	    itembutton.outline_height = 10
+	    self.tv.add_node(itembutton, g)
 
     def cat_clicked(self, button):
         self.tv.toggle_node(button)
